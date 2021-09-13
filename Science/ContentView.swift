a5c4438624f1f09ed64f33b376a7ecc38c9612d3
Abhiram13 <abhiram131869@gmail.com>
2021-09-13 21:45:52 +0530

    login response being fetched

--- Science/ContentView.swift
+++ Science/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 import CoreData
 
-struct LoginBody {
+struct LoginBody: Codable {
     let empid: Int;
     let password: String;
+}
+
+//struct LoginSubmit {
+//    @Binding var username: String;
+//    @Binding var password: String;
+//    private let loginRoute: URL;
+//    private let request: URLRequest;
+//
+//    init(userName: Binding<String>, passWord: Binding<String>) {
+//        self._username = userName;
+//        self._password = passWord;
+//        self.loginRoute = URL(string: "http://localhost:1995/Login")!;
+//        self.request = URLRequest(url: self.loginRoute);
+//    }
+//
+//    request
+//}
+
+func LoginSubmit() -> Void {
+    let route: URL = URL(string: "http://localhost:1995/Login")!
+    var request: URLRequest = URLRequest(url: route);
+    let requestBody: [String: Any] = ["empid": 40005, "password": "123"];
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type");
+    request.httpMethod = "POST";
+    request.httpBody = try? JSONSerialization.data(withJSONObject: requestBody, options: .prettyPrinted)
+    
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            print(error?.localizedDescription ?? "No data")
+            return
+        }
+        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+        if let responseJSON = responseJSON as? [String: Any] {
+            print(responseJSON)
+        }
+    }
+
+    task.resume()
 }
 
 struct LoginForm: View {
@@ -19,6 +57,7 @@ struct LoginForm: View {
     @State private var password: String = "";
     private let screenWidth: CGFloat = UIScreen.main.bounds.width;
     private let screenHeight: CGFloat = UIScreen.main.bounds.height;
+    let v: Void = LoginSubmit();
     var body: some View {
         VStack(spacing: nil) {
             TextField("Employee Id", text: $empid)
