23c3679d867371310da8686eedcb2e6ab44a9953
Abhiram13 <abhiram131869@gmail.com>
2021-09-13 17:24:17 +0530

    initial commit

---
+++ Science/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  Science
+//
+//  Created by Abhiram Nagadi on 12/09/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
