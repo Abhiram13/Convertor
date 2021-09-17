7716e89a7f8c5aec734498dfa76d582da6bf57c3
Abhiram13 <abhiram131869@gmail.com>
2021-09-17 23:57:57 +0530

    commit

--- Science/ContentView.swift
+++ Science/ContentView.swift
@@ -2,86 +2,85 @@ import SwiftUI
 import CoreData
 
 struct LoginResponseBody: Codable {
-    var status: Int?;
-    var response: String = "";
-}
-
-struct LoginRequestBody: Codable {
-    var empid = 40005;
-    var password = "123";
+   var status: Int?;
+   var response: String = "";
 }
 
 struct ContentView: View {
-    @State private var x = LoginResponseBody();
-    @Environment(\.colorScheme) var colorScheme
-    @State private var empid: String = "";
-    @State private var password: String = "";
-    @State private var alert = false;
-    @State private var status = false;
-    let local: UserDefaults = UserDefaults.standard;
-    private let screenWidth: CGFloat = UIScreen.main.bounds.width;
-    private let screenHeight: CGFloat = UIScreen.main.bounds.height;
-    var body: some View {
-        NavigationView {
-            if (status) {
-                NavigationLink(destination: Home(), isActive: self.$status) {
-                    Text("")
-                }
-            } else {
-                VStack(spacing: nil) {
-                    TextField("Employee Id", text: $empid)
-                        .keyboardType(.numberPad)
-                        .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .border((colorScheme == .dark ? Color.white : Color.black), width: 1)
-                        .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                        .multilineTextAlignment(.center)
-                    
-                    TextField("Password", text: $password)
-                        .keyboardType(.namePhonePad)
-                        .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .border((colorScheme == .dark ? Color.white : Color.black), width: 1)
-                        .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                        .multilineTextAlignment(.center)
-                    
-                    Button(action: LoadData) {
-                        Text("Login")
-                            .foregroundColor(.white)
-                            .frame(width: 100, height: 35)
-                    }
-                    .background(Color.blue)
-                    .cornerRadius(8.0)
-                    .alert(isPresented: $alert) {
-                        Alert(title: Text(x.response))
-                    }
-                }
+   @Environment(\.colorScheme) var colorScheme
+   @State private var apiResponse: LoginResponseBody = LoginResponseBody();
+   @State private var empid = "";
+   @State private var password = "";
+   @State private var alert = false;
+   @State private var status = false;
+   private let local: UserDefaults = UserDefaults.standard;
+   private let screenWidth: CGFloat = UIScreen.main.bounds.width;
+   private let screenHeight: CGFloat = UIScreen.main.bounds.height;
+   
+   var body: some View {
+      NavigationView {
+         if (status) {
+            NavigationLink(destination: Home(), isActive: self.$status) {
+               Text("")
             }
-        }.navigationTitle("Login")
-    }
-    
-    func LoadData() -> Void {
-        let route: URL = URL(string: "http://localhost:1995/Login")!
-        var request: URLRequest = URLRequest(url: route);
-        let requestBody: [String: Any?] = ["empid": Int(empid), "password": String(password)];
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type");
-        request.httpMethod = "POST";
-        request.httpBody = try? JSONSerialization.data(withJSONObject: requestBody, options: .prettyPrinted)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                print(error?.localizedDescription ?? "No data")
-                return
+         } else {
+            VStack(alignment: .center, spacing: nil) {
+               Text("Welcome")
+                  .bold()
+                  .font(.system(size: 20))
+                  .foregroundColor(.black)
+               TextField("Employee Id", text: $empid)
+                  .keyboardType(.numberPad)
+                  .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                  .border((colorScheme == .dark ? Color.white : Color.black), width: 1)
+                  .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                  .multilineTextAlignment(.center)
+               
+               TextField("Password", text: $password)
+                  .keyboardType(.namePhonePad)
+                  .frame(width: screenWidth - 50, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                  .border((colorScheme == .dark ? Color.white : Color.black), width: 1)
+                  .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                  .multilineTextAlignment(.center)
+               
+               Button(action: LoadData) {
+                  Text("Login")
+                     .foregroundColor(.white)
+                     .frame(width: 100, height: 35)
+               }
+               .background(Color.blue)
+               .cornerRadius(8.0)
+               .alert(isPresented: $alert) {
+                  Alert(title: Text(apiResponse.response))
+               }
+            }.position(x: screenWidth - (screenWidth/2), y: 10)
+         }
+      }.navigationTitle("Login")
+   }
+   
+   func LoadData() -> Void {
+      let route: URL = URL(string: "http://localhost:1995/Login")!
+      var request: URLRequest = URLRequest(url: route);
+      let requestBody: [String: Any?] = ["empid": Int(empid), "password": String(password)];
+      request.setValue("application/json", forHTTPHeaderField: "Content-Type");
+      request.httpMethod = "POST";
+      request.httpBody = try? JSONSerialization.data(withJSONObject: requestBody, options: .prettyPrinted)
+      
+      URLSession.shared.dataTask(with: request) { data, response, error in
+         guard let data = data, error == nil else {
+            print(error?.localizedDescription ?? "No data")
+            return
+         }
+         
+         if let decodedResponse = try? JSONDecoder().decode(LoginResponseBody.self, from: data) {
+            DispatchQueue.main.async {
+               self.apiResponse = decodedResponse;
+               self.alert = true;
+               self.status = decodedResponse.status == 200 ? true : false;
+               local.set(decodedResponse.response, forKey: "token");
             }
-            
-            if let decodedResponse = try? JSONDecoder().decode(LoginResponseBody.self, from: data) {
-                DispatchQueue.main.async {
-                    self.x = decodedResponse;
-                    self.alert = true;
-                    self.status = decodedResponse.status == 200 ? true : false;
-                    local.set(decodedResponse.response, forKey: "token");
-                }
-                return;
-            }
-        }.resume();
-    }
+            return;
+         }
+      }.resume();
+   }
 }
-
